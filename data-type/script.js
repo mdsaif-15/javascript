7cb401aae4431f30aa3696fa2a96581b5e9a3dfe
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-07-23 19:28:52 +0530

    data type

---
+++ data-type/script.js
@@ -0,0 +1,30 @@
+// Data type 
+
+// primintive data types 
+
+// 1) string ----
+
+
+let name = "Saif";
+
+document.write(typeof (name) + "<br>");
+
+
+// 2) number ----
+
+let Roll = 34;
+document.write(typeof (Roll) + "<br>");
+let num1 = 10;
+let num2 = -10;
+let num3 = 10.002;
+console.log(typeof (num1))
+console.log(typeof (num2))
+console.log(typeof (num3))
+
+
+// 3) big int ----
+
+let bigint = 1232346723n
+
+console.log(typeof (bigint))
+
