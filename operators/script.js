3207a1894489c51ac3fa60fa063a2054280cce43
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-04 11:30:14 +0530

    operators

--- operators/script.js
+++ operators/script.js
@@ -52,3 +52,58 @@ console.log(12===12);
 console.log(12==="12.00");
 console.log(12===13);
 
+// != 
+console.log(12!=12);
+console.log(13!=12);
+
+// !==
+console.log(12!=="12");
+console.log(12!==13);
+
+// >=
+console.log(12>=12);
+console.log(13>=12);
+console.log(10>=12);
+
+// <=
+console.log(12<=12);
+console.log(12<=13);
+console.log(12<=10);
+
+// >
+console.log(13>12);
+console.log(12>12);
+
+// <
+console.log(12<13);
+console.log(12<12);
+
+// 4) Logical operators....
+
+// AND -> "&&"
+
+console.log(true&&true);
+console.log(false&&true);
+console.log(true&&true&&true);
+console.log(true&&false&&true);
+
+// OR -> "||"
+
+console.log(true||false);
+console.log(false||false);
+console.log(true||true);
+
+// NOT -> "!"
+
+console.log(!true);
+console.log(!false);
+
+// 5) Ternary operators....
+
+// ? :
+
+// True/False?If true then this line or code  : else this line or code 
+
+true ? console.log("I am true"):console.log("I am false");
+false ? console.log("I am true"):console.log("I am false");
+
