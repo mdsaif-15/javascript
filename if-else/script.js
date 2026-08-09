6bd96ac8217cd87f31e9280b329de5c584467129
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-09 21:00:42 +0530

    if - else

--- if-else/script.js
+++ if-else/script.js
@@ -1,1 +1,34 @@
-console.log()
+console.log("if - else");
+
+// if , else , elseif....
+
+let isRain = true;
+
+if (isRain == true) {
+    console.log("raining outside");
+}
+else {
+    console.log("not raining outside");
+}
+
+// voting system -
+
+// if age is under 18 print→> not eligible for- vote. 
+// if age is greater than 55 print → over aged.
+// else →> eligible for vote.
+
+let age = prompt("enter the age: ")
+if (age < 18) {
+    console.log("not eligible for vote....");
+}
+else if (age >= 18 && !age > 55) {
+    console.log("eligible for vote....");
+
+}
+else {
+    console.log("over age");
+}
+
+// falsy values -> null, undefined, 0, -1, t'', NaN, On 
+// truthy values-> rest all
+
