f9c6d61087a87d4710a527ca6e7ca9c626b02422
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-16 19:27:39 +0530

    while loop

--- loops/script.js
+++ loops/script.js
@@ -1,8 +1,16 @@
 document.writeln("loop", "</br>");
 
-let t = prompt("enter the number");
+//let t = prompt("enter the number");
 
-for (let i = 1; i <= 10; i++) {
-    document.write(`${t} X ${i} = ${t * i}<br>`);
-    console.log(t + " X " + i + " = " + i * t + "<br>");
+// for (let i = 1; i <= 10; i++) {
+//     document.write(`${t} X ${i} = ${t * i}<br>`);
+//     console.log(t + " X " + i + " = " + i * t + "<br>");
+// }
+
+// while loop----
+
+let i = 0;
+while (i <= 100) {
+    console.log(i);
+    i++;
 }
