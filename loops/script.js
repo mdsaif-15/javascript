7a4a8c6a96b52e5e501a5552147d5f8c34bee7a6
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-16 19:06:32 +0530

    for loop

--- loops/script.js
+++ loops/script.js
@@ -4,5 +4,5 @@ let t = prompt("enter the number");
 
 for (let i = 1; i <= 10; i++) {
     document.write(`${t} X ${i} = ${t * i}<br>`);
-    //document.write(t + " X " + i + " = " + i * t + "<br>");
+    console.log(t + " X " + i + " = " + i * t + "<br>");
 }
